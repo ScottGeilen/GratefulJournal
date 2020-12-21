d7a03c718e1c88226ac43605febf18d1cc21eeca
Scott <scottgeilen99@gmail.com>
2020-12-20 19:30:49 -0500

    Initializing folder hierarchy

---
+++ config/Main.java
@@ -0,0 +1,11 @@
+package grateful;
+import entry.Entry;
+import java.util.*;
+import java.time.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Date d = new Date();
+        d.date();
+    }
+}
---
+++ source/Entry.java
@@ -0,0 +1,10 @@
+package entry;
+import java.util.*;
+import java.time.*;
+
+public class Entry {
+    public static void date() {
+        Date today = Calendar.getInstance().getTime();
+        System.out.println("\n\nToday is " + today);
+    }
+}
